003d4077ef4a4011e1bc9d8080c01431a7ddcebe
SanGeeky <sangeeky@outlook.com>
2021-07-26 23:59:02 -0500

    Add Controller Examples

--- lib/hello_web/controllers/hello_controller.ex
+++ lib/hello_web/controllers/hello_controller.ex
@@ -7,7 +7,31 @@ defmodule HelloWeb.HelloController do
     render(conn, "index.html")
   end
 
-  def show(conn, %{"messenger" => messenger}) do
-    render(conn, "show.html", messenger: messenger)
-  end
+  # def show(conn, %{"messenger" => messenger}) do
+  #   render(conn, "show.html", messenger: messenger)
+  # end
+
+  # Return Text
+  # def show(conn, %{"messenger" => messenger}) do
+  #   text(conn, "From messenger #{messenger}")
+  # end
+
+  # Return JSON
+  # def show(conn, %{"messenger" => messenger}) do
+  #   json(conn, %{id: messenger})
+  # end
+
+  # Return HTML
+  # def show(conn, %{"messenger" => messenger}) do
+  #   html(conn, """
+  #    <html>
+  #      <head>
+  #         <title>Passing a Messenger</title>
+  #      </head>
+  #      <body>
+  #        <p>From messenger #{Plug.HTML.html_escape(messenger)}</p>
+  #      </body>
+  #    </html>
+  #   """)
+  # end
 end
